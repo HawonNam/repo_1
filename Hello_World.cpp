de03bac3949a43d47cac5c965974cee669c3ad86
woni <165345087+HawonNam@users.noreply.github.com>
2025-03-14 13:27:31 +0900

    이슈 해결, 학교 학과 학번 이름 영어로 변경

--- Hello_World.cpp
+++ Hello_World.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-// 한국외국어대학교 컴퓨터공학부 남하원 202402754
+// Department of Computer Engineering, Hankuk University of Foreign Studies
+// Name: Ha-Won Nam, Student ID: 202402754
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
